7ac5e127e71a982b7df75ad7681a060cb9cf5bc4
alanasantana <alana.stc@gmail.com>
2024-01-08 16:19:56 -0300

    mudanca de cor ok

--- Main/Environment/CubeSimpleExtEnv.py
+++ Main/Environment/CubeSimpleExtEnv.py
@@ -12,7 +12,7 @@ class CubeSimpleExtEnv(Environment):
 
         scene_file = 'Main/Scenes/Cubes_Simple_Task_One_Cube.ttt'
 
-        self.cube_blue: Shape | None = None
+        self.cube: Shape | None = None
 
         num_positions = 50
 
@@ -23,21 +23,31 @@ class CubeSimpleExtEnv(Environment):
 
         np.save('positions.npy', self.positions)
 
+        index_sequence = np.tile(np.arange(3), num_positions // 3 + 1)[:num_positions]
+        np.random.shuffle(index_sequence)
+        self.rgb_colors = np.zeros((num_positions, 3))
+        self.rgb_colors[np.arange(num_positions), index_sequence] = 1
+        self.rgb_colors = self.rgb_colors.astype(float)
+
+        np.save('rgb_colors.npy', self.rgb_colors)
+
         # Initialize parent class
-        super().__init__("CubeExtTouch(GenPosition)", scene_file, **kwargs)
+        super().__init__("CubeExtTouch(GenPosition_&_Color)", scene_file, **kwargs)
 
 
     def configure(self) -> None:
         self._load_objects()
         ind = np.random.randint(len(self.positions))
-
         pos = np.round(self.positions[ind], 2)
+        color = list(self.rgb_colors[ind])
 
-        self.cube_blue.set_position([pos[0], pos[1], 0.5345])
+        self.cube.set_position([pos[0], pos[1], 0.5345])
+        self.cube.set_color(color)
+
 
     def reward(self):
         self._load_objects()
-        return self._touch_blue_cube()
+        return self._touch_cube()
 
 
     def observe(self):
@@ -61,22 +71,22 @@ class CubeSimpleExtEnv(Environment):
     def _load_objects(self) -> None:
 
         # Load objects shapes from handles
-        if self.cube_blue is None:
-            self.cube_blue = Shape(name_or_handle=sim.simGetObjectHandle("Cube_Blue"))
+        if self.cube is None:
+            self.cube = Shape(name_or_handle=sim.simGetObjectHandle("Cube_Blue"))
 
     def _get_collisions(self):
 
         self._load_objects()
 
-        n_collision_blue, _ = self.NAO.check_collisions(self.cube_blue)
+        n_collision_blue, _ = self.NAO.check_collisions(self.cube)
 
 
         return n_collision_blue
 
 
-    def _touch_blue_cube(self):
+    def _touch_cube(self):
 
-        n_collision_blue = self._get_collisions()
+        n_collision = self._get_collisions()
 
-        return n_collision_blue
+        return n_collision
 

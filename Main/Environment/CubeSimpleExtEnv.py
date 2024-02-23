bc7045cc7a728570f25d70cb2a95abcf04f4212c
alanasantana <alana.stc@gmail.com>
2024-02-23 15:59:24 -0300

    alteraçoes ok

--- Main/Environment/CubeSimpleExtEnv.py
+++ Main/Environment/CubeSimpleExtEnv.py
@@ -27,23 +27,49 @@ class CubeSimpleExtEnv(Environment):
     def reward(self):
         self._load_objects()
 
-        stack_green_red = abs(self.cube_green.get_position()[0] - self.cube_red.get_position()[0]) < 0.08 and abs(self.cube_green.get_position()[1] - self.cube_red.get_position()[1]) < 0.08
-        stack_green_blue = abs(self.cube_green.get_position()[0] - self.cube_blue.get_position()[0]) < 0.08 and abs(self.cube_green.get_position()[1] - self.cube_blue.get_position()[1]) < 0.08
-        stack_red_blue = abs(self.cube_red.get_position()[0] - self.cube_blue.get_position()[0]) < 0.08 and abs(self.cube_red.get_position()[1] - self.cube_blue.get_position()[1]) < 0.08
+        dist_green_red = (abs(self.cube_green.get_position()[0] - self.cube_red.get_position()[0]) < 0.07 and
+                          abs(self.cube_green.get_position()[1] - self.cube_red.get_position()[1]) < 0.07)
+        dist_green_blue = (abs(self.cube_green.get_position()[0] - self.cube_blue.get_position()[0]) < 0.07 and
+                           abs(self.cube_green.get_position()[1] - self.cube_blue.get_position()[1]) < 0.07)
+        dist_red_blue = (abs(self.cube_red.get_position()[0] - self.cube_blue.get_position()[0]) < 0.07 and
+                         abs(self.cube_red.get_position()[1] - self.cube_blue.get_position()[1]) < 0.07)
+        dist_three_cubes = (abs(self.cube_green.get_position()[0] - self.cube_red.get_position()[0]) < 0.07 and
+        abs(self.cube_green.get_position()[1] - self.cube_red.get_position()[1]) < 0.07) and
+        (abs(self.cube_red.get_position()[0] - self.cube_blue.get_position()[0]) < 0.07 and
+         abs(self.cube_red.get_position()[1] - self.cube_blue.get_position()[1]) < 0.07)
+
+        stack_green_red = dist_green_red and ((self.cube_green.get_position()[2] == 0.5345 and self.cube_red.get_position()[2] > 0.5360) or (self.cube_green.get_position()[2] > 0.5360 and self.cube_red.get_position()[2] == 0.5345))
+        stack_green_blue = dist_green_blue and ((self.cube_green.get_position()[2] == 0.5345 and self.cube_blue.get_position()[2] > 0.5360) or (self.cube_green.get_position()[2] > 0.5360 and self.cube_blue.get_position()[2] == 0.5345))
+        stack_red_blue = dist_red_blue and ((self.cube_red.get_position()[2] == 0.5345 and self.cube_blue.get_position()[2] > 0.5360) or (self.cube_red.get_position()[2] > 0.5360 and self.cube_blue.get_position()[2] == 0.5345))
+
+        stack_green_red_blue = dist_three_cubes and (self.cube_green.get_position()[2] == 0.5345 and self.cube_red.get_position()[2] > self.cube_green.get_position()[2] and self.cube_blue.get_position()[2] > self.cube_red.get_position()[2])
+        stack_green_blue_red = dist_three_cubes and (self.cube_green.get_position()[2] == 0.5345 and self.cube_blue.get_position()[2] > self.cube_green.get_position()[2] and self.cube_red.get_position()[2] > self.cube_blue.get_position()[2])
+        stack_blue_green_red = dist_three_cubes and (self.cube_blue.get_position()[2] == 0.5345 and self.cube_green.get_position()[2] > self.cube_blue.get_position()[2] and self.cube_red.get_position()[2] > self.cube_green.get_position()[2])
+        stack_blue_red_green = dist_three_cubes and (self.cube_blue.get_position()[2] == 0.5345 and self.cube_red.get_position()[2] > self.cube_blue.get_position()[2] and self.cube_green.get_position()[2] > self.cube_red.get_position()[2])
+        stack_red_green_blue = dist_three_cubes and (self.cube_red.get_position()[2] == 0.5345 and self.cube_green.get_position()[2] > self.cube_red.get_position()[2] and self.cube_blue.get_position()[2] > self.cube_green.get_position()[2])
+        stack_red_blue_green = dist_three_cubes and (self.cube_red.get_position()[2] == 0.5345 and self.cube_blue.get_position()[2] > self.cube_red.get_position()[2] and self.cube_green.get_position()[2] > self.cube_blue.get_position()[2])
 
         r = 0.0
-        if stack_green_red and ((self.cube_green.get_position()[2] <= 0.5360 and self.cube_red.get_position()[2] > 0.5360) or (self.cube_green.get_position()[2] > 0.5360 and self.cube_red.get_position()[2] <= 0.5360)):
+        if stack_green_red:
             r += 1.0
-            if stack_red_blue or stack_green_blue:
-                r += 1.0
-        elif stack_green_blue and ((self.cube_green.get_position()[2] <= 0.5360 and self.cube_blue.get_position()[2] > 0.5360) or (self.cube_green.get_position()[2] > 0.5360 and self.cube_blue.get_position()[2] <= 0.5360)):
+        elif stack_green_blue:
             r += 1.0
-            if stack_red_blue or stack_green_red:
-                r += 1.0
-        elif stack_red_blue and ((self.cube_red.get_position()[2] <= 0.5360 and self.cube_blue.get_position()[2] > 0.5360) or (self.cube_red.get_position()[2] > 0.5360 and self.cube_blue.get_position()[2] <= 0.5360)):
+        elif stack_red_blue:
             r += 1.0
-            if stack_green_red or stack_green_blue:
-                r += 1.0
+        elif stack_green_red_blue:
+            r += 1.0
+        elif stack_green_blue_red:
+            r += 1.0
+        elif stack_blue_green_red:
+            r += 1.0
+        elif stack_blue_red_green:
+            r += 1.0
+        elif stack_blue_red_green:
+            r += 1.0
+        elif stack_red_green_blue:
+            r += 1.0
+        elif stack_red_blue_green:
+            r += 1.0
 
         return r
 
